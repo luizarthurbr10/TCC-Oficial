58ba44f4703998d1d70901dac6261f9a1157a7db
luizarthurbr10 <luizarthurmontanher@gmail.com>
2024-09-03 09:22:27 -0400

    3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.masterpage')
+@section('conteudo')
+    HOME
+@endsection
---
+++ resources/views/layouts/masterpage.blade.php
@@ -0,0 +1,9 @@
+@include('layouts.head')
+@include('layouts.header')
+@include('layouts.navBar')
+@include('layouts.carousel')
+
+@yield('conteudo')
+
+@include('layouts.footer')
+@include('layouts.script')
