5c7058eee473f5aa65128f29ff9177243ff90c52
NoletoAna <ananoleto2007@gmail.com>
2024-09-03 21:00:54 -0400

    4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,40 @@
 @extends('layouts.masterpage')
 @section('conteudo')
-    HOME
+    <section>
+        {{-- TITULO DA SESSÃO --}}
+        <div>
+            <h1>Ofertas do Mês</h1>
+        </div>
+
+        {{-- CARDS DE PROMOÇÃO --}}
+        <div id="cards">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/cards/1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/cards/2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/cards/3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
