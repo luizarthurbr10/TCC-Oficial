a3ddd0c768aa109de200462eee1fc7d888600efd
NoletoAna <ananoleto2007@gmail.com>
2024-09-12 20:34:39 -0400

    8

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,74 +1,109 @@
 @extends('layouts.masterpage')
 @section('conteudo')
-
     {{-- BOX BENEFÍCIOS --}}
     <div id="boxBeneficios" class="row p-5">
-        <div class="col">
-            <div class="item card h-100">
-                <div class="textoItem card-body">
-                    <img src="assets/img/icons/cartaoCredito.png" alt="">
-                    <p class="card-text p-2">Parcele Sem Juros no <b>Cartão</b></p>
+
+        <div class="col coluna">
+            <div class="item card rounded-0">
+                <div class="row conteudo card-body p-2 align-self-center">
+                    <div class="col-4">
+                        <img class="icone" src="assets/img/icons/cartaoCredito.png" height="50px" alt="">
+                    </div>
+                    <div class="col texto">
+                        <p class="card-text">Parcele Sem Juros no <b>Cartão</b></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="item card h-100">
-                <div class="textoItem card-body">
-                    <p class="card-text p-2">Entrega de produtos <b>Garantida</b></p>
+
+        <div class="col coluna">
+            <div class="item card rounded-0">
+                <div class="row conteudo card-body p-2 align-self-center">
+                    <div class="col-4">
+                        <img class="icone" src="assets/img/icons/caminhao.png" height="50px" alt="">
+                    </div>
+                    <div class="col">
+                        <p class="card-text">Entrega de produtos <b>Garantida</b></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="item card h-100">
-                <div class="textoItem card-body">
-                    <p class="card-text p-2">Compre no site e <b>Retire na Loja</b></p>
+
+        <div class="col coluna">
+            <div class="item card rounded-0">
+                <div class="row conteudo card-body p-2 align-self-center">
+                    <div class="col-4">
+                        <img class="icone" src="assets/img/icons/loja.png" height="50px" alt="">
+                    </div>
+                    <div class="col">
+                        <p class="card-text">Compre no site e <b>Retire na Loja</b></p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="item card h-100">
-                <div class="textoItem card-body">
-                    <p class="card-text p-2">Tudo para <b>Construir, Reformar e Decorar</b></p>
+
+        <div class="col coluna">
+            <div class="item card rounded-0">
+                <div class="row conteudo card-body p-2 align-self-center">
+                    <div class="col-4">
+                        <img class="icone" src="assets/img/icons/serrote.png" height="50px" alt="">
+                    </div>
+                    <div class="col">
+                        <p class="card-text">Tudo para <b>Construir, Reformar e Decorar</b></p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
+    {{-- CARDS PROMOÇÃO --}}
     <section>
-        {{-- TITULO DA SESSÃO --}}
-        <div>
+        <div id="titulo">
             <h1>Ofertas do Mês</h1>
         </div>
 
-        {{-- CARDS DE PROMOÇÃO --}}
-        <div id="cards" class="alinhamentoDiv">
-            <div class="card" style="width: 18rem;">
-                <img src="assets/img/cards/1.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="card-container">
+            <div class="card">
+                <img src="assets/img/cards/1.jpg" alt="Imagem do Card 1">
+                <div class="card-content">
+                    <h3>Título do Card 1</h3>
+                    <p>Descrição curta do card 1. Este texto pode ser sobre qualquer conteúdo interessante.</p>
+                    <a href="#" class="button">Saiba mais</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
-                <img src="assets/img/cards/2.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card">
+                <img src="https://via.placeholder.com/300x200" alt="Imagem do Card 2">
+                <div class="card-content">
+                    <h3>Título do Card 2</h3>
+                    <p>Descrição curta do card 2. Algum conteúdo adicional interessante pode ser colocado aqui.</p>
+                    <a href="#" class="button">Saiba mais</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
-                <img src="assets/img/cards/3.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card">
+                <img src="https://via.placeholder.com/300x200" alt="Imagem do Card 3">
+                <div class="card-content">
+                    <h3>Título do Card 3</h3>
+                    <p>Descrição curta do card 3. Algo mais que você gostaria de adicionar sobre este card.</p>
+                    <a href="#" class="button">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/300x200" alt="Imagem do Card 3">
+                <div class="card-content">
+                    <h3>Título do Card 3</h3>
+                    <p>Descrição curta do card 3. Algo mais que você gostaria de adicionar sobre este card.</p>
+                    <a href="#" class="button">Saiba mais</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="https://via.placeholder.com/300x200" alt="Imagem do Card 3">
+                <div class="card-content">
+                    <h3>Título do Card 3</h3>
+                    <p>Descrição curta do card 3. Algo mais que você gostaria de adicionar sobre este card.</p>
+                    <a href="#" class="button">Saiba mais</a>
                 </div>
             </div>
         </div>
+    
     </section>
 @endsection
